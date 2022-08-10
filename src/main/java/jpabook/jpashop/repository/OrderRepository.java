b618019a97e23fd4adaa4a4b6c147dc1f49d2d04
Dragonspine <ghoul300@naver.com>
2022-08-10 22:32:01 +0900

    feat: OrderRepository

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+/**
+ * packageName    : jpabook.jpashop.repository
+ * fileName       : OrderRepository
+ * author         : kanghyun Kim
+ * date           : 2022/08/10
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/08/10        kanghyun Kim      최초 생성
+ */
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+}
